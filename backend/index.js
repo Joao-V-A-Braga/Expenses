14f884082799d4ebeac28fb8d9ede2f6f56daac4
jvab <jvab@aluno.ifnmg.edu.br>
2022-02-01 16:01:07 -0300

    Configurando Knex e adicionando Tabela de Usuário

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const app = require('express')()
 const consign = require("consign")
+const db = require('./config/db')
 
+app.db = db
 consign()
     .then('./config/middlewares.js')
     .then('./api')
